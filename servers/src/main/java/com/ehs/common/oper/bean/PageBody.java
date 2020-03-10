dccdd789eb981a52c3a747013a55b1347eb2cb5a
selfvolinghis <2629106344@qq.com>
2020-03-10 16:07:00 +0800

    Update PageBody.java

--- servers/src/main/java/com/ehs/common/oper/bean/PageBody.java
+++ servers/src/main/java/com/ehs/common/oper/bean/PageBody.java
@@ -1,9 +1,23 @@
 package com.ehs.common.oper.bean;
 
+import org.apache.commons.lang.StringUtils;
+import org.springframework.data.domain.Sort;
+
+import com.ehs.common.base.entity.BaseEntity;
+
 public class PageBody {
 
 	private Integer page=1;
 	private Integer size=20;
+	private SortBean sort;
+	
+	
+	public SortBean getSort() {
+		return sort;
+	}
+	public void setSort(SortBean sort) {
+		this.sort = sort;
+	}
 	public Integer getPage() {
 		return page;
 	}
@@ -17,5 +31,18 @@ public class PageBody {
 		this.size = size;
 	}
 	
-	
+	public Sort getSortForJpaQuery() {
+		Sort s = null;
+		if(this.sort!=null) {
+			if (StringUtils.equals(sort.getOrder(), "descending")) {
+				s = new Sort(Sort.Direction.DESC, sort.getProp());
+			} else {
+				s = new Sort(Sort.Direction.ASC, sort.getProp());
+			}
+		}else {
+			s = new Sort(Sort.Direction.DESC, BaseEntity.CREATION_TIME);
+		}
+		return s;
+
+	}
 }
