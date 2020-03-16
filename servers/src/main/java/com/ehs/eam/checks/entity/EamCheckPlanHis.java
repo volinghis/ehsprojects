971d0241d352c68a1095fe860b5b15b6b4c674fc
selfvolinghis <2629106344@qq.com>
2020-03-16 16:12:28 +0800

    Update EamCheckPlanHis.java

--- servers/src/main/java/com/ehs/eam/checks/entity/EamCheckPlanHis.java
+++ servers/src/main/java/com/ehs/eam/checks/entity/EamCheckPlanHis.java
@@ -3,10 +3,12 @@ package com.ehs.eam.checks.entity;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import com.ehs.eam.checks.entity.entitysuper.EamCheckPlanSuper;
+
 
 @Entity
 @Table(name = "EAM_CHECK_PLAN_HIS")
-public class EamCheckPlanHis extends EamCheckPlan{
+public class EamCheckPlanHis extends EamCheckPlanSuper{
 
 	private static final long serialVersionUID = 1L;
 
