48391c311b0d2416be1623dc5fc9e320648687e5
Mapleaves <15229255171@163.com>
2020-03-18 15:03:02 +0800

    修改404页面

--- uis/src/page/404.vue
+++ uis/src/page/404.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>ssss</div>
+<div style="text-align: center;">
+   <el-image :src="dhUrl" fit="fill" style="width:600px;height:600px;"></el-image>
+</div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      dhUrl: require('@/assets/404.svg')
+    }
+  }
+}
+</script>
