e36a732405507ee837b59f6777f0dcc26a2d9c8d
MapLeave <15229255171@163.com>
2020-04-17 17:42:28 +0800

    添加页面模块

--- uis/src/page/portal/home/index.vue
+++ uis/src/page/portal/home/index.vue
@@ -1,5 +1,101 @@
 <template>
   <div>
-    阿斯顿发生的发生的
+    <el-row :gutter="10" style="margin-bottom:10px;">
+      <el-col :span="8">
+        <template>
+          <el-card>
+            <div slot="header" class="clearfix">
+              <span>公司新闻</span>
+              <el-button style="float: right; padding: 3px 0" type="text">更多</el-button>
+            </div>
+            <el-table :data="tableData" style="width: 100%">
+              <el-table-column prop="date" label="日期"> </el-table-column>
+              <el-table-column prop="name" label="姓名"> </el-table-column>
+            </el-table>
+          </el-card>
+        </template>
+      </el-col>
+      <el-col :span="8">
+        <template>
+          <el-card>
+            <div slot="header" class="clearfix">
+              <span>公司公告</span>
+              <el-button style="float: right; padding: 3px 0" type="text">更多</el-button>
+            </div>
+            <el-table :data="tableData" style="width: 100%">
+              <el-table-column prop="date" label="日期"> </el-table-column>
+              <el-table-column prop="name" label="姓名"> </el-table-column>
+            </el-table>
+          </el-card>
+        </template>
+      </el-col>
+      <el-col :span="8">
+        <template>
+          <el-card>
+            <div slot="header" class="clearfix">
+              <span>专题报道</span>
+              <el-button style="float: right; padding: 3px 0" type="text">更多</el-button>
+            </div>
+            <el-table :data="tableData" style="width: 100%">
+              <el-table-column prop="date" label="日期"> </el-table-column>
+              <el-table-column prop="name" label="姓名"> </el-table-column>
+            </el-table>
+          </el-card>
+        </template>
+      </el-col>
+    </el-row>
+    <el-row :gutter="10">
+      <el-col :span="8">
+        <template>
+          <el-card>
+            <div slot="header" class="clearfix">
+              <span>行业新闻</span>
+              <el-button style="float: right; padding: 3px 0" type="text">更多</el-button>
+            </div>
+            <el-table :data="tableData" style="width: 100%">
+              <el-table-column prop="date" label="日期"> </el-table-column>
+              <el-table-column prop="name" label="姓名"> </el-table-column>
+            </el-table>
+          </el-card>
+        </template>
+      </el-col>
+      <el-col :span="8">
+        <template>
+          <el-card>
+            <div slot="header" class="clearfix">
+              <span>招标公告</span>
+              <el-button style="float: right; padding: 3px 0" type="text">更多</el-button>
+            </div>
+            <el-table :data="tableData" style="width: 100%">
+              <el-table-column prop="date" label="日期"> </el-table-column>
+              <el-table-column prop="name" label="姓名"> </el-table-column>
+            </el-table>
+          </el-card>
+        </template>
+      </el-col>
+      <el-col :span="8">
+        <template>
+          <el-card>
+            <div slot="header" class="clearfix">
+              <span>媒体聚焦</span>
+              <el-button style="float: right; padding: 3px 0" type="text">更多</el-button>
+            </div>
+            <el-table :data="tableData" style="width: 100%">
+              <el-table-column prop="date" label="日期"> </el-table-column>
+              <el-table-column prop="name" label="姓名"> </el-table-column>
+            </el-table>
+          </el-card>
+        </template>
+      </el-col>
+    </el-row>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      tableData: []
+    }
+  }
+}
+</script>
