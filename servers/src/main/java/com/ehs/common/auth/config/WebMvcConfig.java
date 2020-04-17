c0869f0e20bc58cc15ab8e7b341b59bcd9031b40
selfvolinghis <2629106344@qq.com>
2020-04-17 16:51:30 +0800

    Update WebMvcConfig.java

--- servers/src/main/java/com/ehs/common/auth/config/WebMvcConfig.java
+++ servers/src/main/java/com/ehs/common/auth/config/WebMvcConfig.java
@@ -7,6 +7,7 @@ import javax.annotation.Resource;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -23,10 +24,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	public void addInterceptors(InterceptorRegistry registry) {
 		String ignorePattens=environment.getProperty("com.ehs.auth.authFilter.ignorePattens");
 		if(StringUtils.isNotBlank(ignorePattens)) {
-			registry.addInterceptor(new RequestAuthInterceptor()).addPathPatterns("/**")
+			InterceptorRegistration ip= registry.addInterceptor(new RequestAuthInterceptor()).addPathPatterns("/**");
+			String[] ss=StringUtils.split(ignorePattens,",");
+			for(String s:ss) {
+				ip.excludePathPatterns(s);
+			}
 			// 去除拦截主页，登录页面，注册页面，忘记密码页面
 			// 静态资源拦截springboot2.1.7 自动配置好的不用设置 （不知道是不是2.X版本都不用设置）
-			.excludePathPatterns(Arrays.asList(ignorePattens));
 		}else {
 			registry.addInterceptor(new RequestAuthInterceptor()).addPathPatterns("/**");
 		}
