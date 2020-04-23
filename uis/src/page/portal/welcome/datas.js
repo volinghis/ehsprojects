8a9f2bd4b56803c6c5f3f688c83ee4f420ad1404
selfvolinghis <2629106344@qq.com>
2020-04-23 15:19:03 +0800

    添加欢迎页

---
+++ uis/src/page/portal/welcome/datas.js
@@ -0,0 +1,10 @@
+export default {
+
+  methods: {
+    loginUser(){
+      var currUser = JSON.parse(sessionStorage.getItem(this.GlobalVars.userToken))
+
+      return currUser.username
+    }
+  }
+}
---
+++ uis/src/page/portal/welcome/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    welcome,{{loginUser()}}!
+
+  </div>
+</template>
+<script>
+import datas from './datas.js'
+export default datas
+
+</script>
+<style lang="scss" scoped>
+@import './styles.scss';
+</style>
