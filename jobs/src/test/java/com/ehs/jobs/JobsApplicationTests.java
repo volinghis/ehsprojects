aaba50e6725ac7cea4a87ebc06c1b2445c08a2ad
selfvolinghis <2629106344@qq.com>
2020-05-13 10:28:08 +0800

    提交代码

---
+++ jobs/src/test/java/com/ehs/jobs/JobsApplicationTests.java
@@ -0,0 +1,30 @@
+package com.ehs.jobs;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JobsApplicationTests {
+
+	
+	
+	@Test
+	public void contextLoads() {
+
+
+	}
+
+}
